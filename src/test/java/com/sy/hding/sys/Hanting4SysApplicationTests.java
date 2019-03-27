bbfbd4943249f53baa6aeae2e5d1eebab0948f1f
TianJingChang <1105450789@qq.com>
2019-03-27 08:05:02 +0800

    田经昌3/27

--- src/test/java/com/sy/hding/sys/Hanting4SysApplicationTests.java
+++ src/test/java/com/sy/hding/sys/Hanting4SysApplicationTests.java
@@ -1,5 +1,8 @@
 package com.sy.hding.sys;
 
+import java.io.*;
+import java.net.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,9 +11,49 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Hanting4SysApplicationTests {
-
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws IOException {
+		BufferedReader br=null;
+			//访问百度
+			URL ur=new URL("http://apis.juhe.cn/mobile/get?phone=19958128041&dtype=&key=61c6a4822158f67a501d6acf5e91a8f1");
+			//获得连接
+			HttpURLConnection http=(HttpURLConnection)ur.openConnection();
+			//设置请求头
+			http.setRequestMethod("GET");
+			//http.setDefaultRequestProperty("C-http", "application/json");
+			http.setConnectTimeout(6*10000);//连接超时
+			http.setReadTimeout(6*1000);//coxket超时
+			//建立连接
+			http.connect();
+			//获得响应信息
+			int code=http.getResponseCode();
+			switch (code) {
+			case HttpURLConnection.HTTP_OK:
+				  System.out.println("资源获取成功");
+				  //读取数据
+				  //低级流转换成高级流
+				 br=new BufferedReader(new InputStreamReader(http.getInputStream(),"UTF-8"));
+				 for(String str=null;(str=br.readLine())!=null;) {
+					 System.out.println(str);
+				 }
+				break;
+			case HttpURLConnection.HTTP_NOT_FOUND:
+				  System.out.println("资源不存在");
+				break;
+			case HttpURLConnection.HTTP_INTERNAL_ERROR:
+				  System.out.println("服务器内部错误");
+				break;
+			default:
+				break;
+			}
+		
+	}
+	
+	@Test
+	public void ok(){
+		new Thread(
+			
+		);
 	}
 
 }
